f9e7e7597022fd58fd878cd1d18b4a838402d5b0
YoungChen <niaomingjian@outlook.com>
2018-03-03 11:16:08 +0800

    Apache Maven Assembly Plugin

---
+++ executable-jar-with-maven-assembly/src/main/java/com/nmj/maven/MainExecutableJar.java
@@ -0,0 +1,11 @@
+package com.nmj.maven;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class MainExecutableJar {
+
+  public static void main(String[] args) {
+    String str = "ABC";
+    System.out.println("Is it empty? " + StringUtils.isEmpty(str));
+  }
+}
